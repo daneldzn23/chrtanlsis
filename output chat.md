45149471df4618634d6daf2d52ffff510dc347eb
Daniel Garcia Antunes <dantunes@nelogica.com.br>
2026-05-06 17:48:29 -0300

    Updates

--- output chat.md
+++
@@ -1,9 +0,0 @@
-# Profit AI Chart Analysis Output
-
-This file stores the analysis results from Profit AI.
-
----
-
-## Analysis History
-
-(Analyses will appear here)
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './index.css';
-import Grafico from './Grafico';
-
-function App() {
-  return <Grafico />;
-}
-
-export default App;
